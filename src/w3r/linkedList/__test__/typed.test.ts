11a94df6d8848a8fbc65c2be8e49e9feef6bc35d
Jack Wright <jack.wright2@sky.uk>
2023-09-29 12:02:04 +0100

    jest-fix-with-examples

---
+++ src/w3r/linkedList/__test__/typed.test.ts
@@ -0,0 +1,5 @@
+describe('Example TYPESCRIPT block', () => {
+  it('Should return true for example test', () => {
+    expect(1 === 1);
+  });
+});
---
+++ src/w3r/newfolder/__test__/test.test.js
@@ -0,0 +1,5 @@
+describe('Example describe block', () => {
+  it('Should return true for example test in newfolder', () => {
+    expect(1 === 1);
+  });
+});
